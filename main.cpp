c9f8114a41778c48e9614175c1f1c59e4ffb89fe
Minguccion <d.lomonte1@studenti.uniba.it>
2024-12-06 12:59:14 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,18 +1,33 @@
 #include <string>
-#include "Dizionario.cpp"
+#include "DizionarioEsteso.cpp"
 
 int main() {
 
-    Dizionario<std::string> dict;
+    DizionarioEsteso<std::string> dict;
 
+    
     dict.inserisci("apple", "fruit");
     dict.inserisci("car", "vehicle");
     dict.inserisci("book", "object");
 
-    std::cout << "Value for 'apple': " << dict.recupera("apple") << "\n";
-    std::cout << "Value for 'car': " << dict.recupera("car") << "\n";
-    std::cout << "Value for 'pen': " << dict.recupera("pen") << "\n";
+    
+    dict.inserisci("bpple", "collision1"); 
+    dict.inserisci("cpple", "collision2"); 
 
+    
+    dict.stampa();
+
+    
+    std::cout << "Value for 'apple': " << dict.recupera("apple") << "\n";
+    std::cout << "Value for 'bpple': " << dict.recupera("bpple") << "\n";
+
+    
+    std::cout << "Does 'car' exist? " << (dict.appartiene("car") ? "Yes" : "No") << "\n";
+    std::cout << "Does 'pen' exist? " << (dict.appartiene("pen") ? "Yes" : "No") << "\n";
+
+    
+    dict.cancella("car");
+    std::cout << "After deletion of 'car':\n";
     dict.stampa();
 
     return 0;
